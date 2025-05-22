5e7a6da5ec5d3e290606fc617df5fcd7cde23fe3
Bernardo <beefreguglia@gmail.com>
2025-05-22 09:02:03 -0300

    feature: create badge component

---
+++ react-2025/to-do/src/components/badge.tsx
@@ -0,0 +1,48 @@
+import type { ComponentProps } from "react";
+import { Text } from "./text";
+import { cva, type VariantProps } from "class-variance-authority";
+
+export const badgeVariants = cva("", {
+	variants: {
+		variant: {
+			primary: "bg-green-light",
+			secondary: "bg-pink-light",
+		},
+		size: {
+			sm: "py-0.5 px-2",
+		},
+	},
+	defaultVariants: {
+		size: "sm",
+		variant: "primary",
+	},
+});
+
+export const badgeTextVariants = cva("", {
+	variants: {
+		variant: {
+			primary: "text-green-dark",
+			secondary: "text-pink-dark",
+		},
+	},
+});
+type BadgeProps = ComponentProps<"div"> & VariantProps<typeof badgeVariants>;
+
+export function Badge({
+	variant,
+	size,
+	className,
+	children,
+	...props
+}: BadgeProps) {
+	return (
+		<div className={badgeTextVariants({ variant, className })} {...props}>
+			<Text
+				variant="body-sm-bold"
+				className={badgeVariants({ variant, size, className })}
+			>
+				{children}
+			</Text>
+		</div>
+	);
+}
